c72969a7a3ef7a80f8f061d405bc0a706bdf8b8b
syihwamoza <mozakastella26@gmail.com>
2024-05-31 10:58:42 +0700

    jika FRONT_poition > REAR_position iterasi dari FRONT hingga akhir array

--- CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
+++ CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
@@ -69,6 +69,14 @@ public:
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
             }
+
+            cout << endl;
+        }
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
         }
     }
 };
