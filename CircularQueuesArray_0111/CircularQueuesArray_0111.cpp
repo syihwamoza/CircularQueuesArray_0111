ebcf3809011aa9b86ed8d4067cf26a6501ed18c8
syihwamoza <mozakastella26@gmail.com>
2024-05-31 10:55:02 +0700

    jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
+++ CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
@@ -61,6 +61,15 @@ public:
             cout << "Queue is empty\n";
             return;
         }
+
+        cout << "\n Elements in the queue are.... \n";
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+        }
     }
 };
 
