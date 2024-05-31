36497abd04327fb81d5d1920ccb83be08e51c496
syihwamoza <mozakastella26@gmail.com>
2024-05-31 10:35:35 +0700

    jika REAR berada di posisi terakhir array, kembali ke array awal

--- CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
+++ CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
@@ -25,6 +25,12 @@ public:
             FRONT = 0;
             REAR  0;
         }
+        else {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
     }
 };
 int main()
