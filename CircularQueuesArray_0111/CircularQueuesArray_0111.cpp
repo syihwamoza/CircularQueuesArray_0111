38ab2fe66e6b7148fa55a7feb30f657a88296fea
syihwamoza <mozakastella26@gmail.com>
2024-05-31 11:07:21 +0700

    memasukkna switch

--- CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
+++ CircularQueuesArray_0111/CircularQueuesArray_0111.cpp
@@ -53,7 +53,7 @@ public:
         }
     }
 
-    void display() {
+    void diplay() {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
 
@@ -90,5 +90,23 @@ public:
 };
 
 int main() {
+    Queues q;
+    char ch;
 
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+
+            }
+        }
+    }
 }
